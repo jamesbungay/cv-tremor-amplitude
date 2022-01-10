eb93383539409157ac8a0da379224c63c5136813
James Bungay <jtb.github@gmail.com>
2022-01-10 22:33:08 +0000

    Load video into cv2 and threshold each frame

--- pendulum_amplitude.py
+++ pendulum_amplitude.py
@@ -1,21 +1,63 @@
 # -*- coding: utf-8 -*-
 
 import matplotlib.pyplot as plt
+import cv2
 
 
-CAMERA_FOCAL_LENGTH = 2.87
-CAMERA_FOCAL_LENGTH_STD = 32
+CAMERA_FOCAL_LENGTH = 2.87  # mm
+CAMERA_FOCAL_LENGTH_STD = 32  # mm
 
-OBJECT_DEPTH = 40
+OBJECT_DEPTH = 40  # cm
+
+VIDEO_WIDTH = 1080
+VIDEO_FILEPATH = './IMG_1224.mov'
+START_FRAME = 570
+END_FRAME = 9999
 
 
 # -----------------------------------------------------------------------------
-# C A M E R A  R E L A T E D  F U N C T I O N S
+# V I D E O  P R O C E S S I N G
+
+def handleFrame(frame):
+    cv2.imwrite('1.jpg', frame)
+    grayscaled = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(grayscaled, (5, 5), 0)
+    (T, thresholded) = cv2.threshold(blurred, 50, 255, cv2.THRESH_BINARY)
+    cv2.imwrite('2.jpg', thresholded)
+    
+    # TODO detect blob and find midpoint. Return this value
+
+
+def processVideo():
+    minLeft = 0
+    maxRight = VIDEO_WIDTH
+    
+    capture = cv2.VideoCapture(VIDEO_FILEPATH)
+    frameN = 0
+    while (True):
+        success, frame = capture.read()
+        if success:
+            frameN += 1
+            if START_FRAME <= frameN <= END_FRAME:
+                handleFrame(frame)
+                break
+        else:
+            break
+    capture.release()
+    print(frameN)
+    
+    pixelDistance = maxRight - minLeft
+    return pixelDistance
+
+
+# -----------------------------------------------------------------------------
+# C A M E R A  P A R A M E T E R  C A L C U L A T I O N
+
+# TODO derive these constants, rather than hardcoding
 
 CAMERA_SENSOR_WIDTH = 2.328
 CAMERA_SENSOR_HEIGHT = 3.104
 
-VIDEO_WIDTH = 1080
 
 def calcPixelSize(imageResolution, sensorWidth, lensFocalLength, depth):
     """
@@ -58,10 +100,13 @@ def main():
 
     # Note that amplitude here is peak-to-trough distance, as that is the
     # standard for tremor amplitude measurement:
-    amplitudePixelDistance = 1080  # TODO: implement. currently just outputs width of the view.
-    amplitude = amplitudePixelDistance * pixelSize
+    amplitudePixelDistance = processVideo()
+    amplitude = amplitudePixelDistance * pixelSize / 10
 
-    print("Pendulum amplitude (peak-to-trough) = %fcm" % amplitude)
+    depthError = "N"  # TODO implement
+
+    print("Pendulum amplitude (peak-to-trough) = %f +/- %s cm"
+          % (amplitude, depthError))
 
 
 main()
