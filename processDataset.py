e2f570f6355ec40083699ec830b97547159c7614
James Bungay <jtb.github@gmail.com>
2022-03-16 14:05:10 +0000

    Add script to process entire dataset automatically

---
+++ processDataset.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_0.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_0.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_0.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_0.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_1.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_1.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_1.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_1.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_2.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_2.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_2.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_2.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_5.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_5.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_5.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_5.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_10.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_10.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_10.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_50_10.MOV 50 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_0.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_0.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_0.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_0.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_1.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_1.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_1.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_1.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_2.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_2.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_2.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_2.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_5.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_5.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_5.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_5.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_10.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_10.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_10.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_75_10.MOV 75 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_0.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_0.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_0.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_0.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_1.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_1.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_1.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_1.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_2.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_2.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_2.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_2.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_5.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_5.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_5.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_5.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_10.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_10.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_10.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_j_100_10.MOV 100 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_0.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_0.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_0.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_0.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_1.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_1.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_1.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_1.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_2.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_2.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_2.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_2.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_5.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_5.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_5.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_5.MOV 50 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_10.MOV 50 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_10.MOV 50 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_10.MOV 50 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_50_10.MOV 50 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_0.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_0.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_0.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_0.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_1.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_1.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_1.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_1.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_2.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_2.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_2.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_2.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_5.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_5.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_5.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_5.MOV 75 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_10.MOV 75 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_10.MOV 75 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_10.MOV 75 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_75_10.MOV 75 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_0.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_0.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_0.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_0.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_1.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_1.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_1.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_1.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_2.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_2.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_2.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_2.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_5.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_5.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_5.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_5.MOV 100 resting TIP')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_10.MOV 100 resting MCP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_10.MOV 100 resting PIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_10.MOV 100 resting DIP')
+os.system('python3 hand_tremor_amplitude.py data/phase3/resting_o_100_10.MOV 100 resting TIP')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_0.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_0.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_1.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_1.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_2.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_2.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_5.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_5.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_10.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_50_10.MOV 50 postural index')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_0.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_0.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_1.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_1.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_2.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_2.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_5.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_5.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_10.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_75_10.MOV 75 postural index')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_0.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_0.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_1.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_1.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_2.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_2.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_5.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_5.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_10.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_j_100_10.MOV 100 postural index')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_0.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_0.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_1.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_1.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_2.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_2.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_5.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_5.MOV 50 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_10.MOV 50 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_50_10.MOV 50 postural index')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_0.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_0.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_1.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_1.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_2.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_2.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_5.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_5.MOV 75 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_10.MOV 75 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_75_10.MOV 75 postural index')
+
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_0.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_0.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_1.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_1.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_2.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_2.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_5.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_5.MOV 100 postural index')
+
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_10.MOV 100 postural thumb')
+os.system('python3 hand_tremor_amplitude.py data/phase3/postural_o_100_10.MOV 100 postural index')
